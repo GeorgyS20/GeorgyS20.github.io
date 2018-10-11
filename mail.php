f849112143ebad147c1973067823224db0f51729
GeorgyS20 <42900046+GeorgyS20@users.noreply.github.com>
2018-10-11 06:44:23 +0300

    s

--- mail.php
+++ mail.php
@@ -13,7 +13,7 @@ $email = $_POST['user_email'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'lizkon22@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Username = 'lizkon22'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'ae413Aru'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
